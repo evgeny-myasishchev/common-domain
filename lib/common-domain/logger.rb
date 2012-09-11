15e445d7b538b1d3f96606e3c344c4282b59b232
Evgeny Myasishchev <evgeny.myasishchev@gmail.com>
2012-09-11 16:26:22 +0300

    Adding logger factory. Correcting logging so default factory is used without any extra configuration.

--- lib/common-domain/logger.rb
+++ lib/common-domain/logger.rb
@@ -1,8 +1,33 @@
 module CommonDomain
   class Logger
-    class << self
+    
+    #Uses default ruby logger and writes to STDOUT
+    class DefaultFactory
+      def get(name)
+        require 'logger'
+        logger = ::Logger.new(STDOUT)
+        logger.formatter = proc do |severity, datetime, progname, msg|
+          "[#{datetime} #{name} #{severity}]: #{msg}\n"
+        end
+        logger
+      end
+    end
+    
+    class Log4rFactory
       def get(name)
         Log4r::Logger[name] || Log4r::Logger.new(name)
+      end
+    end
+    
+    class << self
+      attr_writer :factory
+      
+      def get(name)
+        factory.get(name)
+      end
+      
+      def factory
+        @factory ||= DefaultFactory.new
       end
     end
   end
