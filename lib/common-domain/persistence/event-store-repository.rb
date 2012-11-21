768747c9b56844b8a901a27e8b1f53a612f408ef
Evgeny Myasishchev <evgeny.myasishchev@gmail.com>
2012-11-21 23:39:10 +0200

    Implementing event store specific implementation of the work.

--- lib/common-domain/persistence/event-store-repository.rb
+++ lib/common-domain/persistence/event-store-repository.rb
@@ -1,21 +1,42 @@
 module CommonDomain::Persistence
   class EventStoreRepository < Repository
     Log = CommonDomain::Logger.get("common-domain::persistence::event-store-repository")
-    def initialize(event_store, builder)
-      @event_store = event_store
+    
+    class EventStoreWork < AbstractWork
+      attr_reader :repository
+      def initialize(event_store, builder)
+        @aggregates = {}
+        @work = event_store.begin_work
+        @repository = EventStoreRepository.new @work, builder
+      end
+      
+      def get_by_id(aggregate_class, id)
+        # We don't maintain identify map using aggregate_class + id because id is normally a guid which is unlikelly to duplicate
+        return @aggregates[id] if @aggregates.key?(id)
+        @aggregates[id] = @repository.get_by_id(aggregate_class, id)
+      end
+      
+      def commit_changes(headers = {})
+        @aggregates.values.each { |aggregate| @repository.save(aggregate) }
+        @work.commit_changes headers
+      end
+    end
+    
+    def initialize(stream_opener, builder)
+      @stream_opener = stream_opener
       @builder     = builder
     end
     
     def get_by_id(aggregate_class, aggregate_id)
       aggregate = @builder.build(aggregate_class, aggregate_id)
-      stream    = @event_store.open_stream(aggregate_id)
+      stream    = @stream_opener.open_stream(aggregate_id)
       stream.committed_events.each { |event| aggregate.apply_event(event.body) }
       aggregate
     end
     
     def save(aggregate, headers = {})
       Log.debug "Saving aggregate: #{aggregate.aggregate_id}"
-      stream = @event_store.open_stream(aggregate.aggregate_id)
+      stream = @stream_opener.open_stream(aggregate.aggregate_id)
       uncommitted_events = aggregate.get_uncommitted_events
       Log.debug "#{uncommitted_events.length} uncommitted events to commit..."
       uncommitted_events.each { |event|  
@@ -26,5 +47,10 @@ module CommonDomain::Persistence
       Log.debug "Done..."
       aggregate
     end
+    
+    protected
+      def create_work
+        EventStoreWork.new @stream_opener, @builder
+      end
   end
 end
---
+++ spec/xtms-common-domain/persistence/event-store-work-spec.rb
@@ -0,0 +1,71 @@
+require 'spec-helper'
+
+describe CommonDomain::Persistence::EventStoreRepository::EventStoreWork do
+  let(:event_store_work) { mock(:event_store_work) }
+  let(:event_store) { mock(:event_store, begin_work: event_store_work) }
+  let(:repository) { mock(:event_store_repository) }
+  let(:builder) { mock(:builder) }
+  
+  before(:each) do
+    CommonDomain::Persistence::EventStoreRepository.stub(:new) { repository }
+  end
+  
+  subject { described_class.new event_store, builder }
+  
+  describe "initialize" do
+    it "should use event_store to begin new event store work and create event store repository with it" do
+      event_store.should_receive(:begin_work).and_return(event_store_work)
+      CommonDomain::Persistence::EventStoreRepository.should_receive(:new).
+        with(event_store_work, builder).and_return(repository)
+      subject = described_class.new event_store, builder
+      subject.repository.should be repository
+    end
+  end
+  
+  describe "get_by_id" do
+    let(:aggregate) { mock(:aggregate) }
+    let(:aggregate_class) { mock(:aggregate_class) }
+    
+    it "should use repository to get the aggregate" do
+      repository.should_receive(:get_by_id).with(aggregate_class, 'aggregate-8820').and_return(aggregate)
+      subject.get_by_id(aggregate_class, 'aggregate-8820').should be aggregate
+    end
+    
+    it "should return same instance of the aggregate without accessing the repo" do
+      repository.should_receive(:get_by_id).once.and_return(aggregate)
+      subject.get_by_id(aggregate_class, 'aggregate-8820').should be aggregate
+      subject.get_by_id(aggregate_class, 'aggregate-8820').should be aggregate
+    end
+  end
+  
+  describe "commit_changes" do
+    let(:aggregate_1) { mock(:aggregate_1) }
+    let(:aggregate_2) { mock(:aggregate_2) }
+    before(:each) do
+      repository.stub(:get_by_id).with(anything, 'aggregate-1').and_return(aggregate_1)
+      repository.stub(:get_by_id).with(anything, 'aggregate-2').and_return(aggregate_2)
+      subject.get_by_id(nil, 'aggregate-1')
+      subject.get_by_id(nil, 'aggregate-2')
+      
+      repository.stub(:save)
+      event_store_work.stub(:commit_changes)
+    end
+    
+    it "should use repository to commit changes of all retrieved aggregates and then commit the work of event store" do
+      repository.should_receive(:save).with(aggregate_1)
+      repository.should_receive(:save).with(aggregate_2)
+      event_store_work.should_receive(:commit_changes)
+      subject.commit_changes
+    end
+    
+    it "should commit changes of event store work with headers" do
+      headers = {header1: 'header-1'}
+      event_store_work.should_receive(:commit_changes).with(headers)
+      subject.commit_changes headers
+    end
+    
+    it "should return nil" do
+      subject.commit_changes.should be_nil
+    end
+  end
+end
