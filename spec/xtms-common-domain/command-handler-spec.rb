9d99333172f43e57414436a70c5e7db76ecd8bea
Evgeny Myasishchev <evgeny.myasishchev@gmail.com>
2012-11-26 09:36:24 +0200

    Adding spec stub.

--- spec/xtms-common-domain/command-handler-spec.rb
+++ spec/xtms-common-domain/command-handler-spec.rb
@@ -4,4 +4,6 @@ describe CommonDomain::CommandHandler do
   it "should be a kind of a MessagesHandler" do
     subject.should be_a_kind_of CommonDomain::Infrastructure::MessagesHandler
   end
+  
+  it "should be possible to define message handlers that will be wrapped into begin_work"
 end
