97f65147e6166a3b132572055a34ecc37d54b9cf
Evgeny Myasishchev <evgeny.myasishchev@gmail.com>
2013-01-04 18:58:11 +0200

    Adding extra matcher.

--- lib/rspec/common-domain/work-matchers.rb
+++ lib/rspec/common-domain/work-matchers.rb
@@ -1,33 +1,50 @@
-RSpec::Matchers.define :begin_work do
-  match do |repository|
-    work = mock(:work)
-    repository.should_receive(:begin_work) do |headers = {}, &block|
-      block.call(work)
-    end
-    work
+module RSpec::Matchers::CommonDomainMatchers
+  def self.setup_work_mock(mock)
+    mock.stub(on_committed: nil)
   end
-end
-
-RSpec::Matchers.define :begin_work_with_headers do |headers|
-  raise "Headers must be supplied" if headers.nil?
-  match do |repository|
-    work = mock(:work)
-    repository.should_receive(:begin_work) do |h, &block|
-      h.should eql headers
-      block.call(work) unless block.nil?
+    
+  RSpec::Matchers.define :begin_work do
+    match do |repository|
+      work = mock(:work)
+      RSpec::Matchers::CommonDomainMatchers::setup_work_mock work
+      repository.should_receive(:begin_work) do |headers = {}, &block|
+        block.call(work)
+      end
+      work
     end
-    work
   end
-end
 
-
-# Sample:
-#   work.should get_and_return_aggregate(Acount, 'account-993').and_return(account_instance)
-RSpec::Matchers.define :get_and_return_aggregate do |aggregate_class, aggregate_id, aggregate|
-  raise "aggregate_class should be supplied" if aggregate_class.nil?
-  raise "aggregate_id should be supplied" if aggregate_id.nil?
+  RSpec::Matchers.define :begin_work_with_headers do |headers|
+    raise "Headers must be supplied" if headers.nil?
+    match do |repository|
+      work = mock(:work)
+      RSpec::Matchers::CommonDomainMatchers::setup_work_mock work
+      repository.should_receive(:begin_work) do |h, &block|
+        h.should eql headers
+        block.call(work) unless block.nil?
+      end
+      work
+    end
+  end
   
-  match do |repo_or_work|    
-    repo_or_work.should_receive(:get_by_id).with(aggregate_class, aggregate_id).and_return(aggregate)
+  RSpec::Matchers.define :register_on_committed do
+    match do |work|
+      on_committed = nil
+      work.should_receive(:on_committed) do |&block|
+        on_committed = block
+      end
+      lambda { on_committed.call unless on_committed.nil? }
+    end
+  end
+  
+  # Sample:
+  #   work.should get_and_return_aggregate(Acount, 'account-993').and_return(account_instance)
+  RSpec::Matchers.define :get_and_return_aggregate do |aggregate_class, aggregate_id, aggregate|
+    raise "aggregate_class should be supplied" if aggregate_class.nil?
+    raise "aggregate_id should be supplied" if aggregate_id.nil?
+
+    match do |repo_or_work|    
+      repo_or_work.should_receive(:get_by_id).with(aggregate_class, aggregate_id).and_return(aggregate)
+    end
   end
 end
--- spec/rspec/work-matchers-spec.rb
+++ spec/rspec/work-matchers-spec.rb
@@ -9,6 +9,12 @@ describe "work-matchers" do
       work.should be_instance_of RSpec::Mocks::Mock
       expect { |block| repository.begin_work(&block) }.to yield_with_args(work)
     end
+    
+    it "should stub on_committed" do
+      work = repository.should begin_work
+      work.on_committed.should be_nil
+      repository.begin_work { }
+    end
   end
   
   describe "begin_work_with_headers" do
@@ -28,6 +34,12 @@ describe "work-matchers" do
       work = repository.should begin_work_with_headers headers
       lambda { repository.begin_work({wrong: 'header-100'}) }.should raise_error(RSpec::Expectations::ExpectationNotMetError)
     end
+    
+    it "should stub on_committed" do
+      work = repository.should begin_work_with_headers({header: 'header-1'})
+      work.on_committed.should be_nil
+      repository.begin_work({header: 'header-1'}) { }
+    end
   end
   
   describe "get_and_return_aggregate" do
@@ -43,6 +55,23 @@ describe "work-matchers" do
     it "should setup get_by_id with aggregate_class and aggregate_id and return aggregate_instance" do
       work.should get_and_return_aggregate(aggregate_class, 'aggregate-100', aggregate_instance)
       work.get_by_id(aggregate_class, 'aggregate-100').should be aggregate_instance
+    end
+  end
+  
+  describe "register_on_committed" do
+    let(:work) { mock(:work) }
+    it "should setup on_committed expectation and return a callback to trigger committed callback" do
+      callback = work.should register_on_committed
+      expect { |block|
+        work.on_committed &block
+        callback.call
+      }.to yield_control
+    end
+    
+    it "should call the block only if the callback was registered" do
+      callback = work.should register_on_committed
+      lambda { callback.call }.should_not raise_error
+      work.on_committed {}
     end
   end
 end
