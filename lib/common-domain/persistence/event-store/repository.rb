865d3a6d9df788f1e06625ebf6de3a9a09e91db7
Evgeny Myasishchev <evgeny.myasishchev@gmail.com>
2013-02-01 23:08:27 +0200

    Naming adjustments.

--- lib/common-domain/persistence/event-store/repository.rb
+++ lib/common-domain/persistence/event-store/repository.rb
@@ -4,13 +4,13 @@ module CommonDomain::Persistence::EventStore
     
     Log = CommonDomain::Logger.get("common-domain::persistence::event-store::repository")
     
-    def initialize(stream_opener, builder)
-      @stream_opener = stream_opener
+    def initialize(event_store, builder)
+      @event_store = event_store
       @builder = builder
     end
     
     def get_by_id(aggregate_class, aggregate_id)
-      stream = @stream_opener.open_stream(aggregate_id)
+      stream = @event_store.open_stream(aggregate_id)
       raise CommonDomain::Persistence::AggregateNotFoundError.new(aggregate_class, aggregate_id) if stream.new_stream?
       aggregate = @builder.build(aggregate_class, aggregate_id)
       stream.committed_events.each { |event| aggregate.apply_event(event.body) }
@@ -19,7 +19,7 @@ module CommonDomain::Persistence::EventStore
     
     def save(aggregate, headers = {})
       Log.debug "Saving aggregate '#{aggregate.aggregate_id}'..."
-      flush_changes aggregate, @stream_opener do |stream|
+      flush_changes aggregate, @event_store do |stream|
         Log.debug "Committing changes..."
         stream.commit_changes headers
       end
@@ -29,7 +29,7 @@ module CommonDomain::Persistence::EventStore
     
     protected
       def create_work
-        Work.new @stream_opener, @builder
+        Work.new @event_store, @builder
       end
   end
 end
