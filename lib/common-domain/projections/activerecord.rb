464d7284a581d6ccd036af026061cc33a9360470
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2014-05-06 14:23:21 +0200

    Adding logic to configure the projection.

--- lib/common-domain/projections/activerecord.rb
+++ lib/common-domain/projections/activerecord.rb
@@ -2,7 +2,7 @@ module CommonDomain::Projections
   module ActiveRecord
     require 'active_record'
     
-    # The model is used to hold various connection related meta information
+    # The model is used to hold various projection related meta information
     # like version
     class ProjectionsMeta < ::ActiveRecord::Base
       def self.ensure_schema!
@@ -10,8 +10,30 @@ module CommonDomain::Projections
       end
     end
     
+    module ClassMethods
+      def config
+        @config ||= default_config
+      end
+      
+      # Configure the projection. Available options:
+      # - version <number> - The version of the projection. Should be incremented if projection the projection needs rebubild.
+      #   The rebuild is usually required if schema has changed.
+      # - identifier <value> - The identifier or the projection. Usually assigned automatically based on the AR model table name
+      def projection(config = {})
+        @config = default_config.merge config
+      end
+      
+      def default_config
+        {
+          version: 0,
+          identifier: self.table_name
+        }
+      end
+    end
+    
     def self.included(receiver)
       receiver.extend CommonDomain::Projections::Base
+      receiver.extend ClassMethods
     end
   end
 end
--- spec/xtms-common-domain/projections/activerecord-projection-spec.rb
+++ spec/xtms-common-domain/projections/activerecord-projection-spec.rb
@@ -19,8 +19,31 @@ describe CommonDomain::Projections::ActiveRecord do
     include CommonDomain::Projections::ActiveRecord
   end
   
+  subject { TheProjection }
+  
   it "should be a base projection" do
     TheProjection.should be_a CommonDomain::Projections::Base
+  end
+  
+  describe "projection config" do
+    class ProjectionConfigSpec < ActiveRecord::Base
+      include CommonDomain::Projections::ActiveRecord
+    end
+    subject { ProjectionConfigSpec }
+    
+    it "should have default config" do
+      subject.config.should eql version: 0, identifier: 'projection_config_specs'
+    end
+    
+    describe "configured" do
+      before(:each) do
+        subject.projection version: 100, identifier: 'some-identifier-100'
+      end
+      
+      it "should assign specific values" do
+        subject.config.should eql version: 100, identifier: 'some-identifier-100'
+      end
+    end
   end
   
   describe CommonDomain::Projections::ActiveRecord::ProjectionsMeta do
