1b84f6771390a50c16856be1fca29a3f4dc13c63
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2014-05-06 13:26:43 +0200

    Adding meta class stubs

--- lib/common-domain/projections/activerecord.rb
+++ lib/common-domain/projections/activerecord.rb
@@ -1,5 +1,15 @@
 module CommonDomain::Projections
   module ActiveRecord
+    require 'active_record'
+    
+    # The model is used to hold various connection related meta information
+    # like version
+    class ProjectionsMeta < ::ActiveRecord::Base
+      def self.ensure_schema!
+        
+      end
+    end
+    
     def self.included(receiver)
       receiver.extend CommonDomain::Projections::Base
     end
--- spec/xtms-common-domain/projections/activerecord-projection-spec.rb
+++ spec/xtms-common-domain/projections/activerecord-projection-spec.rb
@@ -3,11 +3,32 @@ require 'spec-helper'
 describe CommonDomain::Projections::ActiveRecord do
   require 'active_record'
   
+  before(:all) do
+    @db_path = @tmp_root.join('ar-projections-spec.sqlite')
+    ActiveRecord::Base.establish_connection(
+      adapter: "sqlite3",
+      database: @db_path
+    )
+  end
+  
+  after(:all) do
+    ActiveRecord::Base.remove_connection
+  end
+  
   class TheProjection < ActiveRecord::Base
     include CommonDomain::Projections::ActiveRecord
   end
   
   it "should be a base projection" do
     TheProjection.should be_a CommonDomain::Projections::Base
+  end
+  
+  describe CommonDomain::Projections::ActiveRecord::ProjectionsMeta do
+    let(:described_class) { CommonDomain::Projections::ActiveRecord::ProjectionsMeta }
+    # subject { CommonDomain::Projections::ActiveRecord::ProjectionsMeta.new }
+    it "should be active record" do
+      # puts "@tmp_root: #{tmp_root}"
+      # subject.should be_a ::ActiveRecord::Base
+    end
   end
 end
