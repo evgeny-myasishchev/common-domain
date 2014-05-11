777fb164e53b8ed11e74a00d031b1ddfec035782
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2014-05-11 15:30:52 +0300

    Adding some logging.

--- spec/support/activerecord-helpers.rb
+++ spec/support/activerecord-helpers.rb
@@ -2,7 +2,9 @@ module ActiveRecordHelpers
   module ClassMethods
     def use_sqlite_activerecord_connection(database_name)
       before(:all) do
+        logger = CommonDomain::Logger.factory.get('spec')
         @db_path = @tmp_root.join(database_name)
+        logger.info("Establishing ActiveRecord sqlite3 connection. Database path: #{@db_path}")
         ActiveRecord::Base.establish_connection(
           adapter: "sqlite3",
           database: @db_path
