cb1b900b61bd5f900648874369fd5bc4fe93c20d
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2014-10-04 17:08:11 +0300

    Reading database connection specification from file. Making it possible to specify the file via env.

--- spec/support/sql-connection-helper.rb
+++ spec/support/sql-connection-helper.rb
@@ -1,6 +1,6 @@
 module SqlConnectionHelper
   def sqlite_memory_connection(logger_name = "common-domain::spec::orm")
-    con = Sequel.connect adapter: "sqlite", database: ":memory:" 
+    con = Sequel.connect RSpec.configuration.database_config, {:orm_log_level => :debug}
     con.loggers << CommonDomain::Logger.get(logger_name)
     con
   end
