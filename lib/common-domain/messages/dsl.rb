585d82669373e09b2c1e64ba14188ecfe7343845
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2015-04-25 14:26:22 +0300

    Implementing DSL to define message.

--- lib/common-domain/messages/dsl.rb
+++ lib/common-domain/messages/dsl.rb
@@ -1,3 +1,14 @@
 module CommonDomain::Messages::Dsl
+  module ClassMethods
+    def message(const_name, *args)
+      event_class = Class.new(CommonDomain::Messages::Message) do
+        attr_reader *args
+      end
+      const_set(const_name, event_class)
+    end
+  end
   
+  def self.included(receiver)
+    receiver.extend ClassMethods
+  end
 end
--- spec/common-domain/messages/dsl-spec.rb
+++ spec/common-domain/messages/dsl-spec.rb
@@ -1,10 +1,25 @@
-describe CommonDomain::Messages::Dsl do
-  let(:events_module) {
-    Module.new do
-      include CommonDomain::Messages::Dsl
+require 'spec-helper'
+
+module CommonDomainMessagesDslSpec
+  module Messages
+    include CommonDomain::Messages::Dsl
+    message :NoAttributesMessage
+    message :SimpleMessage, :login, :password
+  end
+  
+  describe CommonDomain::Messages::Dsl do
+    describe 'message' do
+      it 'should define message without any attribute' do
+        expect(Messages.const_defined?(:NoAttributesMessage)).to be_truthy
+        expect(Messages::NoAttributesMessage.new).to be_a CommonDomain::Messages::Message
+        expect(Messages::NoAttributesMessage.attribute_names).to be_empty
+      end
       
-      message :NoAttributesMessage
-      message :SimpleMessage, :login, :password
+      it 'should define simple message with provided attributes' do
+        expect(Messages.const_defined?(:SimpleMessage)).to be_truthy
+        expect(Messages::SimpleMessage.new({})).to be_a CommonDomain::Messages::Message
+        expect(Messages::SimpleMessage.attribute_names).to eql [:login, :password]
+      end
     end
-  }
+  end
 end
