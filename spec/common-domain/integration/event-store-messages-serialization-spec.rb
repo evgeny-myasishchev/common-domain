f2f125a40402c044b709a6ba3d0ba67401a3384b
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2015-04-27 00:22:59 +0300

    Extracting common spec to a shared examples.

--- spec/common-domain/integration/event-store-messages-serialization-spec.rb
+++ spec/common-domain/integration/event-store-messages-serialization-spec.rb
@@ -7,22 +7,22 @@ module EventStoreSerializationSpec
   end
   
   describe "Integration - Event Store - Serialization" do
-    describe 'yaml' do
+    shared_examples_for 'serializer' do
       it 'should serialize/deserialize the message' do
-        subject = EventStore::Persistence::Serializers::YamlSerializer.new
         msg = Messages::EmployeeCreated.new 'emp-1', 'Employee 1', 'mail@employee-1.com'
         data = subject.serialize msg
         expect(subject.deserialize(data)).to eql msg
       end
     end
     
+    describe 'yaml' do
+      subject { EventStore::Persistence::Serializers::YamlSerializer.new }
+      it_behaves_like 'serializer'
+    end
+    
     describe 'json' do
-      it 'should serialize/deserialize the message' do
-        subject = EventStore::Persistence::Serializers::JsonSerializer.new
-        msg = Messages::EmployeeCreated.new 'emp-1', 'Employee 1', 'mail@employee-1.com'
-        data = subject.serialize msg
-        expect(subject.deserialize(data)).to eql msg
-      end
+      subject { EventStore::Persistence::Serializers::JsonSerializer.new }
+      it_behaves_like 'serializer'
     end
   end
 end
