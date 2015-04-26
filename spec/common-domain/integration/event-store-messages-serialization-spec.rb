18147775912b31bafd2a3f4363bcbe30b4da032d
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2015-04-26 20:24:43 +0300

    Adding integration spec to make sure serialization works.

---
+++ spec/common-domain/integration/event-store-messages-serialization-spec.rb
@@ -0,0 +1,19 @@
+require 'spec-helper'
+
+module EventStoreSerializationSpec
+  module Messages
+    include CommonDomain::Messages::Dsl
+    message :EmployeeCreated, :aggregate_id, :full_name, :email
+  end
+  
+  describe "Integration - Event Store - Serialization" do
+    describe 'yaml' do
+      it 'should serialize/deserialize the message' do
+        subject = EventStore::Persistence::Serializers::YamlSerializer.new
+        msg = Messages::EmployeeCreated.new 'emp-1', 'Employee 1', 'mail@employee-1.com'
+        data = subject.serialize msg
+        expect(subject.deserialize(data)).to eql msg
+      end
+    end
+  end
+end
