7763f1f482ec440924d75ba7f7ac932f85feae97
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2015-04-29 00:57:46 +0200

    Using messages framework for domain events.

--- lib/common-domain/domain-event.rb
+++ lib/common-domain/domain-event.rb
@@ -1,4 +1,17 @@
 module CommonDomain
   class DomainEvent < CommonDomain::Messages::Message
+    module DSL
+      def self.included(receiver)
+        receiver.class_eval do
+          include CommonDomain::Messages::DSL
+          setup_dsl message_base_class: CommonDomain::DomainEvent, dsl_module: CommonDomain::DomainEvent::DSL
+
+          class << self
+            alias_method :event, :message
+            alias_method :events_group, :group
+          end
+        end
+      end
+    end
   end
 end
--- spec/common-domain/domain-event-dsl-spec.rb
+++ spec/common-domain/domain-event-dsl-spec.rb
@@ -10,39 +10,12 @@ describe CommonDomain::DomainEvent::DSL do
   describe "event" do
     before(:each) do
       subject.class_eval do
-        event :AccountCreated, :login_name, :email_address
-        event :AccountRemoved
+        event :AccountCreated
       end
     end
     
-    it "should define a new constant withing enclosed module" do
-      expect(subject.const_defined?(:AccountRemoved)).to be_truthy
-    end
-    
     it "should inherit the constant from DomainEvent class" do
-      expect(subject::AccountRemoved.superclass).to be CommonDomain::DomainEvent
-    end
-    
-    it "should define reader attributes" do
-      expect(subject::AccountCreated.method_defined?(:login_name)).to be_truthy
-      expect(subject::AccountCreated.method_defined?(:email_address)).to be_truthy
-    end
-    
-    it "should define initializer that initializes attributes" do
-      instance = subject::AccountCreated.new "aggregate-100", "some login name", "some@email.com"
-      expect(instance.aggregate_id).to eql "aggregate-100"
-      expect(instance.login_name).to eql "some login name"
-      expect(instance.email_address).to eql "some@email.com"
-    end
-    
-    it "should be able to initialize if there are no attributes" do
-      instance = subject::AccountRemoved.new "aggregate-100"
-      expect(instance.aggregate_id).to eql "aggregate-100"
-    end
-    
-    it "should fail to initialize if number of attributes is different than declared" do
-      expect(lambda { subject::AccountCreated.new "aggregate-100" }).to raise_error("Failed to instantiate the event. Expected 3 arguments, got 1")
-      expect(lambda { subject::AccountCreated.new "aggregate-100", 'hello' }).to raise_error("Failed to instantiate the event. Expected 3 arguments, got 2")
+      expect(subject::AccountCreated.superclass).to be CommonDomain::DomainEvent
     end
   end
   
@@ -53,22 +26,15 @@ describe CommonDomain::DomainEvent::DSL do
         
         events_group :AccountEvents do
           event :AccountCreated
-          event :AccountRemoved
         end
       end
     }
     
-    it "should define a new module" do
-      expect(subject.const_defined?(:AccountEvents)).to be_truthy
-    end
-    
     it "should include DSL in the module" do
       expect(subject::AccountEvents.included_modules).to include(CommonDomain::DomainEvent::DSL)
-    end
-    
-    it "should eval passed block so events can be defined" do
       expect(subject::AccountEvents.const_defined?(:AccountCreated)).to be_truthy
-      expect(subject::AccountEvents.const_defined?(:AccountRemoved)).to be_truthy
+      expect(subject::AccountEvents::AccountCreated.superclass).to be CommonDomain::DomainEvent
+
     end
   end
 end
