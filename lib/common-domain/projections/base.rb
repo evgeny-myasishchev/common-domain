4b4f82b2a36566f1b6594b60eb946fa35eaf5562
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2015-11-23 23:37:04 +0200

    Removing unneeded stuff.

--- lib/common-domain/projections/base.rb
+++ lib/common-domain/projections/base.rb
@@ -1,24 +1,11 @@
 module CommonDomain::Projections
   module Base
-    #Setup persistence related stuff (like schema or any other means)
-    def setup
-      raise "Not implemented"
+    def identifier
+      self.class.name
     end
     
-    #Remove everything related to the projection (like data and possible schema).
-    def cleanup!
-      raise "Not implemented"
-    end
-    
-    #Returns true if the projection requires rebuild.
-    #It maybe because of the underlying persistence details has changed.
-    def rebuild_required?
-      raise "Not implemented"
-    end
-    
-    #Returns true if projection requires setup. 
-    #That is if the projection has never been setup before.
-    def setup_required?
+    #Purge projection related data
+    def purge!
       raise "Not implemented"
     end
     
--- spec/common-domain/projections/base-projection-spec.rb
+++ spec/common-domain/projections/base-projection-spec.rb
@@ -1,13 +1,19 @@
 require 'spec-helper'
 
-describe CommonDomain::Projections::Base do
-  let(:described_class) {  
-    Class.new do
-      include CommonDomain::Projections::Base
+module CommonDomainProjectionsBaseSpec
+  class DummyProjection
+    include CommonDomain::Projections::Base
+  end
+
+  describe CommonDomain::Projections::Base do
+    subject { DummyProjection.new }
+
+    it "should be a MessagesHandler" do
+      expect(subject).to be_a(CommonDomain::Messages::MessagesHandler)
     end
-  }
-  
-  it "should be a MessagesHandler" do
-    expect(subject).to be_a(CommonDomain::Messages::MessagesHandler)
+    
+    it 'should maintain identifier based on the full class name' do
+      expect(subject.identifier).to eql 'CommonDomainProjectionsBaseSpec::DummyProjection'
+    end
   end
 end
