0acf2254e4c880d4b695a745d4d3f2b4d04c955d
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2015-11-24 01:04:43 +0200

    Simplifying AR projection.

--- lib/common-domain/projections/activerecord.rb
+++ lib/common-domain/projections/activerecord.rb
@@ -33,100 +33,38 @@ module CommonDomain::Projections
   module ActiveRecord
     require 'active_record'
     
-    # The model is used to hold various projection related meta information
-    # like version
-    class ProjectionsMeta < ::ActiveRecord::Base
-      class << self
-        def ensure_schema!
-          unless connection.table_exists? table_name
-            connection.create_table(table_name) do |t|
-              t.column :projection_id, :string, null: false
-              t.column :version, :integer, null: false
-            end
-          end
-        end
-        
-        def setup_required?(projection_id)
-          return true unless table_exists?
-          !exists? projection_id: projection_id
-        end
-        
-        def rebuild_required?(projection_id, version)
-          return false unless table_exists?
-          meta = find_by projection_id: projection_id
-          return false if meta.nil?
-          return true if version > meta.version
-          return false if version == meta.version
-          raise "Downgrade is not supported for projection #{projection_id}. Last known version is #{meta.version}. Requested projection version was #{version}."
-        end
-      end
-    end
-    
-    class Projection
-      include Base
-      
-      attr_reader :config, :model
-      
-      def initialize(model, config)
-        @model = model
-        @config = config
+    module ProjectionInstanceMethods
+      def initialize(model_class)
+        @model_class = model_class
       end
       
-      def setup
-        ProjectionsMeta.ensure_schema!
-        raise "Projection '#{config[:identifier]}' has already been initialized." if ProjectionsMeta.exists?(projection_id: config[:identifier])
-        ProjectionsMeta.create! projection_id: config[:identifier], version: config[:version]
+      def identifier
+        @model_class.name
       end
       
-      def cleanup!
-        model.transaction do
-          model.delete_all
-          ProjectionsMeta.where(projection_id: config[:identifier]).delete_all
-        end
-      end
-      
-      def rebuild_required?
-        result = ProjectionsMeta.rebuild_required?(config[:identifier], config[:version])
-        result
-      end
-      
-      def setup_required?
-        result = ProjectionsMeta.setup_required?(config[:identifier])
-        result
+      def purge!
+        @model_class.delete_all
       end
     end
     
     module ClassMethods
-      def projection_config
-        @projection_config ||= default_projection_config
-      end
-      
-      # Configure the projection. Available options:
-      # - version <number> - The version of the projection. Should be incremented if projection the projection needs rebubild.
-      #   The rebuild is usually required if schema has changed.
-      # - identifier <value> - The identifier or the projection. Usually assigned automatically based on the AR model table name
-      def projection(projection_config = {}, &block)
-        @projection_config = default_projection_config.merge projection_config
+      def projection(&block)
         @projection_init_block = block
         self.send(:remove_const, 'Projection') unless @projection_class.nil?
         @projection_class = nil
       end
       
-      def default_projection_config
-        {
-          version: 0,
-          identifier: self.table_name
-        }
-      end
-      
       def create_projection(*args)
         @projection_class ||= begin
-          projection_class = Class.new(Projection)
+          projection_class = Class.new do
+            include Base
+            include ProjectionInstanceMethods
+          end
           projection_class.class_exec(*args, &@projection_init_block) if @projection_init_block
-          self.const_set("Projection", projection_class)
+          self.const_set(:Projection, projection_class)
           projection_class
         end
-        @projection_class.new self, projection_config
+        @projection_class.new self
       end
     end
     
--- spec/common-domain/projections/activerecord-projection-spec.rb
+++ spec/common-domain/projections/activerecord-projection-spec.rb
@@ -4,30 +4,23 @@ describe CommonDomain::Projections::ActiveRecord do
   include ActiveRecordHelpers
   establish_activerecord_connection
   
-  class TheProjection < ActiveRecord::Base
+  class ProjectionModel < ActiveRecord::Base
     include CommonDomain::Projections::ActiveRecord
   end
   
-  subject { TheProjection }
-  let(:meta_class) { CommonDomain::Projections::ActiveRecord::ProjectionsMeta }
+  subject { ProjectionModel.create_projection }
   
   before(:each) do
     c = ActiveRecord::Base.connection
-    c.drop_table 'projections_meta' if c.table_exists? 'projections_meta'
   end
   
-  describe "create_projection" do
-    class ProjectionConfigSpec < ActiveRecord::Base
-      include CommonDomain::Projections::ActiveRecord
-    end
-    subject { ProjectionConfigSpec.create_projection }
-    
-    it "should create and configure instance of the projection class" do
-      expect(subject).to be_a CommonDomain::Projections::ActiveRecord::Projection
+  describe 'projection instance' do
+    it 'should create instance of the projection class' do
+      expect(subject).to be_a ProjectionModel::Projection
     end
     
-    it "should invoke the projection block in context of projection class" do
-      ProjectionConfigSpec.projection do
+    it 'should invoke the projection block in context of projection class' do
+      ProjectionModel.projection do
         def self.get_the_self
           self
         end
@@ -40,164 +33,56 @@ describe CommonDomain::Projections::ActiveRecord do
       expect(subject.get_the_self).to eql subject
     end
     
-    it "should supply create_projection args to the projection configuration block" do
-      ProjectionConfigSpec.projection do |*args|
+    it 'should supply create_projection args to the projection configuration block' do
+      ProjectionModel.projection do |*args|
         @method_args = args
         def self.the_args
           @method_args
         end
       end
-      projection = ProjectionConfigSpec.create_projection 100, "arg-2", "arg-3"
+      projection = ProjectionModel.create_projection 100, "arg-2", "arg-3"
       expect(projection.class.the_args).to eql [100, 'arg-2', 'arg-3']
     end
     
-    it "should have default config" do
-      expect(subject.config).to eql version: 0, identifier: 'projection_config_specs'
-    end
-    
-    describe "configured" do
-      before(:each) do
-        ProjectionConfigSpec.projection version: 100, identifier: 'some-identifier-100'
-      end
-      
-      it "should assign specific values" do
-        expect(subject.config).to eql version: 100, identifier: 'some-identifier-100'
-      end
+    it 'should use model class name as an identifier' do
+      expect(subject.identifier).to eql ProjectionModel.name
     end
   end
   
-  describe "setup" do
-    before(:each) do
-      subject.projection version: 110, identifier: "projection-110"
-      subject.create_projection.setup
-    end
-    
-    after(:each) do
-      meta_class.where(projection_id: 'projection-110').delete_all
-    end
-    
-    it "should setup schema of the meta " do
-      class ProjectionConfigSetupSpec1 < ActiveRecord::Base
-        include CommonDomain::Projections::ActiveRecord
-      end
-      expect(meta_class).to receive(:ensure_schema!).and_call_original
-      ProjectionConfigSetupSpec1.create_projection.setup
-    end
-    
-    it "should raise error if initialized before" do
-      expect(lambda { subject.create_projection.setup }).to raise_error("Projection 'projection-110' has already been initialized.")
-    end
-    
-    it "should record corresponding record" do
-      meta = meta_class.find_by projection_id: 'projection-110'
-      expect(meta).not_to be_nil
-      expect(meta.version).to eql 110
+  it 'should create class once' do
+    expect(subject.class).to eql ProjectionModel.create_projection.class
+  end
+  
+  it 'should assign const for the class' do
+    expect(subject.class.name).to eql 'ProjectionModel::Projection'
+  end
+  
+  describe 'purge!' do
+    it 'it should delete all' do
+      expect(ProjectionModel).to receive(:delete_all)
+      ProjectionModel.create_projection.purge!
     end
   end
   
-  describe "cleanup!" do
-    class ActiveRecordProjectionCleanupSpec < ActiveRecord::Base
-      include CommonDomain::Projections::ActiveRecord
-      def self.ensure_schema!
-        unless connection.table_exists? table_name
-          connection.create_table(table_name) do |t|
-            t.column :name, :string
-          end
-        end
-      end
-    end
-    subject { ActiveRecordProjectionCleanupSpec }
-    let(:model_class) { ActiveRecordProjectionCleanupSpec }
-    
-    before(:each) do
-      subject.projection version: 120, identifier: "projection-120"
-      projection = subject.create_projection
-      projection.setup
-      model_class.ensure_schema!
-      model_class.create! name: 'Name 1'
-      model_class.create! name: 'Name 2'
-      model_class.create! name: 'Name 3'
-      projection.cleanup!
-    end
-    
-    it "should delete all data" do
-      expect(model_class.count).to eql 0
-    end
-    
-    it "should delete corresponding meta record" do
-      expect(meta_class.find_by(projection_id: 'projection-120')).to be_nil
-    end
-  end
-  
-  describe "rebuild_required?" do
-    before(:each) do
-      allow(meta_class).to receive(:table_exists?) { true }
-      subject.projection version: 130, identifier: "projection-130"
-    end
-    
-    it "should use meta model to define that" do
-      expect(meta_class).to receive(:rebuild_required?).with('projection-130', 130).and_return(true)
-      expect(subject.create_projection.rebuild_required?).to be_truthy
-    end
-        
-    it "should be false if meta model table does not exist" do
-      expect(meta_class).to receive(:table_exists?) { false }
-      expect(subject.create_projection.rebuild_required?).to be_falsey
-    end
-  end
-  
-  describe "setup_required?" do
-    before(:each) do
-      subject.projection version: 130, identifier: "projection-130"
-      allow(meta_class).to receive(:table_exists?) { true }
-    end
-    
-    it "should use meta model to define that" do
-      expect(meta_class).to receive(:setup_required?).with('projection-130').and_return(true)
-      expect(subject.create_projection.setup_required?).to be_truthy
-    end
-    
-    it "should be true if meta model table does not exist" do
-      expect(meta_class).to receive(:table_exists?) { false }
-      expect(subject.create_projection.setup_required?).to be_truthy
-    end
-  end
-  
-  describe "events_handling" do
+  describe 'events_handling' do
     module ArProjectionSpecEvents
       include CommonDomain::DomainEvent::DSL
       event :EmployeeCreated, :aggregate_id
       event :EmployeeChanged, :aggregate_id
       event :EmployeeRemoved, :aggregate_id
     end
-    class EventsHandlingSpec < ActiveRecord::Base
-      include CommonDomain::Projections::ActiveRecord
-      projection do
-        def handled_events
-          @handled_events ||= []
-        end
-        
-        on ArProjectionSpecEvents::EmployeeCreated do |event|
-          handled_events << event
-        end
-        
-        on ArProjectionSpecEvents::EmployeeRemoved do |event|
-          handled_events << event
+    
+    it 'should handle events' do
+      ProjectionModel.class_eval do
+        projection do
+          def handled_events
+            @handled_events ||= []
+          end
+          on(ArProjectionSpecEvents::EmployeeCreated) { |event| handled_events << event }
+          on(ArProjectionSpecEvents::EmployeeRemoved) { |event| handled_events << event }
         end
       end
-    end
-    
-    subject { EventsHandlingSpec.create_projection }
-    
-    it "should create class once" do
-      expect(subject.class).to eql EventsHandlingSpec.create_projection.class
-    end
-    
-    it "should assign const to the class" do
-      expect(subject.class.name).to eql 'EventsHandlingSpec::Projection'
-    end
-    
-    it "should handle events" do
+      
       e1 = ArProjectionSpecEvents::EmployeeCreated.new('e-1')
       e2 = ArProjectionSpecEvents::EmployeeChanged.new('e-2')
       e3 = ArProjectionSpecEvents::EmployeeRemoved.new('e-3')
