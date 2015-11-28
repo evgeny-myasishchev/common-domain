77f4c90ba77f4d4df06154d8ac773c019c1afb4b
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2015-11-28 13:26:35 +0200

    Extending persistence factory. Adding hooks support.

--- lib/common-domain/persistence-factory.rb
+++ lib/common-domain/persistence-factory.rb
@@ -2,17 +2,29 @@ module CommonDomain
   class PersistenceFactory
     def initialize(event_store, builder, snapshots_repository = nil)
       @event_store, @builder, @snapshots_repository = event_store, builder, snapshots_repository
+      @hooks = []
+    end
+    
+    def hook **hook
+      @hooks << hook
     end
     
     def create_repository
-      Persistence::Repository.new(@event_store, @builder, @snapshots_repository)
+      repo = Persistence::Repository.new(@event_store, @builder, @snapshots_repository)
+      add_hooks repo
+      repo
     end
 
     def begin_unit_of_work(headers, &block)
       uow = CommonDomain::UnitOfWork.new(create_repository)
+      add_hooks uow
       result = yield(uow)
       uow.commit headers
       result
+    end
+    
+    private def add_hooks(receiver)
+      @hooks.each { |hook| receiver.hook hook }
     end
   end
 end
--- spec/common-domain/persistence/persistence-factory-spec.rb
+++ spec/common-domain/persistence/persistence-factory-spec.rb
@@ -11,10 +11,23 @@ describe CommonDomain::PersistenceFactory do
     it 'should create a new instance of the repository' do
       expect(subject.create_repository).to be_an_instance_of(CommonDomain::Persistence::Repository)
     end
+    
+    it 'should add all registered hooks to a newly created repository' do
+      hook1 = {after_commit: -> {}}
+      hook2 = {after_commit: -> {}}
+      subject.hook hook1
+      subject.hook hook2
+      
+      repo = instance_double(CommonDomain::Persistence::Repository)
+      expect(CommonDomain::Persistence::Repository).to receive(:new) { repo }
+      expect(repo).to receive(:hook).with hook1
+      expect(repo).to receive(:hook).with hook2
+      subject.create_repository
+    end
   end
 
   describe 'begin_unit_of_work' do
-    let(:repository) { instance_double(CommonDomain::Persistence::Repository) }    
+    let(:repository) { instance_double(CommonDomain::Persistence::Repository) }
     let(:uow) { instance_double(CommonDomain::UnitOfWork, commit: nil) }
     
     before do
@@ -31,6 +44,19 @@ describe CommonDomain::PersistenceFactory do
     
     it 'should return block return value' do
       expect(subject.begin_unit_of_work(dummy_headers) {|uow| 100 }).to eql(100)
+    end
+    
+    it 'should add all registered hooks to a newly created unit of work' do
+      hook1 = {after_commit: -> {}}
+      hook2 = {after_commit: -> {}}
+      subject.hook hook1
+      subject.hook hook2
+      
+      uow = instance_double(CommonDomain::UnitOfWork, commit: nil)
+      expect(CommonDomain::UnitOfWork).to receive(:new) { uow }
+      expect(uow).to receive(:hook).with hook1
+      expect(uow).to receive(:hook).with hook2
+      subject.begin_unit_of_work({}) {}
     end
   end
 end
